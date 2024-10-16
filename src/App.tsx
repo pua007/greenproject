942725df173c7ea5260d7b7af894365c10ffaed3
pua007 <pua002@naver.com>
2024-10-16 12:16:26 +0900

    2024-10-15 12:16 second-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from '@pages/index/index';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route index path="/" element={}></Route>
+                <Route index path="/" element={<MainPage />}></Route>
             </Routes>
         </BrowserRouter>
     );
